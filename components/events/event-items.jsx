32ac09bfec231643382e8047c3fdfb280eaddc13
JonathanArriazu <joni.arriazu2@gmail.com>
2023-08-07 17:09:17 -0300

    fix: img replaced with Image from NextJs

--- components/events/event-items.jsx
+++ components/events/event-items.jsx
@@ -5,6 +5,7 @@ import Button from '../ui/button'
 import DateIcon from '../icons/date-icon'
 import AddressIcon from '../icons/address-icon'
 import ArrowRightIcon from '../icons/arrow-right-icon'
+import Image from 'next/image'
 
 const EventItems = (props) => {
 
@@ -22,7 +23,8 @@ const exploreLink = `/events/${id}`
 
   return (
     <li className={styles.item}>
-        <img src={ '/' + image} alt={title} />
+        {/* <img src={ '/' + image} alt={title} /> */}
+        <Image src={'/' + image} alt={title} width={340} height={160} />
         <div className={styles.content}>
             <div className={styles.summary}>
                 <h2>{title}</h2>
